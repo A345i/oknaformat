74a8c9c5b55a53e94192cf28bcee5eeab65a9201
A345i <82287724+A345i@users.noreply.github.com>
2023-05-03 23:09:23 +0300

    Add Litebox

---
+++ js/site.js
@@ -0,0 +1,46 @@
+﻿// Write your JavaScript code.
+
+var dlb = jQuery.noConflict();
+dlb(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    dlb(this).ekkoLightbox();
+});
+
+
+var st = jQuery.noConflict();
+st(function () {
+    st.fn.scrollToTop = function () {
+        st(this).hide().removeAttr("href");
+        if (st(window).scrollTop() != "0") {
+            st(this).fadeIn("slow")
+        }
+        var scrollDiv = st(this);
+        st(window).scroll(function () {
+            if (st(window).scrollTop() == "0") {
+                st(scrollDiv).fadeOut("slow")
+            } else {
+                st(scrollDiv).fadeIn("slow")
+            }
+        });
+        st(this).click(function () {
+            st("html, body").animate({ scrollTop: 0 }, "slow")
+        })
+    }
+});
+st(function () { st("#toTop").scrollToTop(); });
+
+
+var sb = jQuery.noConflict();
+
+//sb("#menu-toggle").click(function (e) {
+//    e.preventDefault();
+//    sb("#wrapper").toggleClass("toggled");
+//});
+
+
+sb(document).on('click', '[data-toggle="sidebar"]', function (event) {
+    event.preventDefault();
+    sb("#wrapper").toggleClass("toggled");
+});
+
+
